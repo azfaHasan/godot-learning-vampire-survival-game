50657e9575f5999f2c448e760ad3ef9e7b987b3c
Alif <deboxers244@gmail.com>
2025-07-12 12:28:38 +0700

    helath-system-update
    - add progress bar
    - add collision damage
    - add signal to end the game

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal health_depleted
+
 var health = 100.0
 
 func _physics_process(delta: float) -> void:
@@ -11,3 +13,12 @@ func _physics_process(delta: float) -> void:
 		$HappyBoo.play_walk_animation()
 	else:
 		$HappyBoo.play_idle_animation()
+	
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		$ProgressBar.value = health
+		
+		if health <= 0.0:
+			health_depleted.emit()
